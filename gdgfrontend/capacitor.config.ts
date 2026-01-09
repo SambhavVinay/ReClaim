b717a2c9373c954d1f543ac219485f73eb5c5cb4
SambhavVinay <sambhavvinay2005@gmail.com>
2026-01-09 14:49:39 +0530

    code checks

--- gdgfrontend/capacitor.config.ts
+++ gdgfrontend/capacitor.config.ts
@@ -4,6 +4,10 @@ const config: CapacitorConfig = {
   appId: "com.lostfound.app",
   appName: "reclaim",
   webDir: "out",
+  server: {
+    cleartext: false,
+    allowNavigation: ["campus-search-api-680513043824.us-central1.run.app"],
+  },
 };
 
 export default config;
